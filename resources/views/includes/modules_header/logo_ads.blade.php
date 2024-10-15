73ef7fa426abcf9ce5be62d8eafb09556786b181
MANOUDEV <manouadjanor@gmail.com>
2024-10-15 10:29:27 +0000

    [ADD] Affichage de l'entête et pied de page des visiteurs.

---
+++ resources/views/includes/modules_header/logo_ads.blade.php
@@ -0,0 +1,21 @@
+<!-- Navbar logo section START -->
+<div>
+    <div class="container">
+        <div class="d-sm-flex justify-content-sm-between align-items-sm-center my-2">
+            <!-- Logo START -->
+            <a class="navbar-brand d-block" href="/">
+                <img class="navbar-brand-item light-mode-item" src="/assets/images/logo.png" alt="logo togoactualite" style="height: 60px;">
+                <img class="navbar-brand-item dark-mode-item" src="/assets/images/blanc.png" alt="logo togoactualite" style="height: 60px;">
+
+            </a>
+            <!-- Logo END -->
+            
+           <div>
+                <a href="#" class="card-img-flash d-block">
+                    <img src="/assets/images/adv-2.png" alt="adv">
+                </a>
+            </div>
+            
+        </div>
+    </div>
+</div>
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>@yield('title') | Togo actualité</title>
+        {!! SEOMeta::generate() !!}
+        {!! OpenGraph::generate() !!}
+        {!! Twitter::generate() !!}
+        {!! JsonLd::generate() !!}
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="/assets/images/Icone.png">
+
+        <!-- Google Font -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&family=Rubik:wght@400;500;700&display=swap" rel="stylesheet">
+
+        <!-- Plugins CSS -->
+        <link rel="stylesheet" type="text/css" href="/assets/vendor/font-awesome/css/all.min.css">
+        <link rel="stylesheet" type="text/css" href="/assets/vendor/bootstrap-icons/bootstrap-icons.css">
+        <!-- Theme CSS -->
+        <link id="style-switch" rel="stylesheet" type="text/css" href="/assets/css/style.css">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
+   </head>
+   <body>
+
+
+       @yield('content')
+ 
+       <!-- Bootstrap JS -->
+       <script defer src="/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+       <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+       
+       <script>
+           navigator.serviceWorker.register("{{ URL::asset('service-worker.js') }}");
+
+           function askForPermission() {
+               Notification.requestPermission().then((permission) => {
+                   if (permission === 'granted') {
+                       // get service worker
+                       navigator.serviceWorker.ready.then((sw) => {
+                           // subscribe
+                           sw.pushManager.subscribe({
+                               userVisibleOnly: true,
+                               applicationServerKey: "BFkgfzx0SjrHdtcwkpzdqbKWFnEmQe08sulnj3JdPLxxZtIwb0Sq8Iz-Os9s54MTATN6EkKKTNyoaujCMuKrlJU"
+                           }).then((subscription) => {
+
+                               saveSub(JSON.stringify(subscription));
+                           });
+                       });
+                   }
+               });
+           }
+
+           function saveSub(sub) {
+               $.ajax({
+                   type: 'post',
+                   url: '{{ URL('/api/frontoffice/save-push-notification-sub') }}',
+                   data: {
+                       '_token': "{{ csrf_token() }}",
+                       'sub': sub
+                   },
+                   success: function(data) {
+                       // cosnole.log(sub)
+                   }
+               });
+           }
+
+           askForPermission()
+ 
+       </script>
+   </body>
+</html>
