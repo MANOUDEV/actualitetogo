707a91105c862dd4ace70c4a41cd626ad69e47cf
MANOUDEV <manouadjanor@gmail.com>
2024-12-16 20:05:43 +0000

    [ADD] Installation des librairies et packages.

---
+++ config/adsense.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'client_id' => 'ca-pub-5594963864359932', //Your Adsense client ID e.g. ca-pub-9508939161510421
+    'ads' => [
+        'responsive' => [
+            'ad_slot' => 4473479121,
+            'ad_format' => 'auto',
+            'ad_full_width_responsive' => true
+        ],
+        'rectangle' => [
+            'ad_slot' => 4473479121,
+            'ad_style' => 'display:inline-block;width:300px;height:250px',
+        ]
+    ]
+];
