f47fa594d36c335c81495f942492936a5212dbd3
MANOUDEV <manouadjanor@gmail.com>
2024-12-18 17:51:38 +0000

    [ADD] Intégration des fichiers assets, models, seeders et migration de la base de données.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,25 +9,31 @@
     <main>
        
         <section class="position-relative" style="margin-top: -45px; margin-bottom: -25px">
-            
-            <div class="row g-4">
-                <div class="col-lg-12 col-sm-12 col-xs-12">
-                    {!! Adsense::show('responsive') !!}
+            <div class="container">
+                <div class="row g-4">
+                    <div class="col-lg-12 col-sm-12 col-xs-12">
+                        {!! Adsense::show('responsive') !!}
+                    </div>
                 </div>
             </div>
+            
             @include('sectionHomePage.alaUne.alaUne')
 
-            <div class="row g-4">
-                <div class="col-lg-12 col-sm-12 col-xs-12">
-                    {!! Adsense::show('responsive') !!}
+            <div class="container">
+                <div class="row g-4">
+                    <div class="col-lg-12 col-sm-12 col-xs-12">
+                        {!! Adsense::show('responsive') !!}
+                    </div>
                 </div>
             </div>
            
             @include('sectionHomePage.toutelActualite.toutelActualite')
 
-            <div class="row g-4">
-                <div class="col-lg-12 col-sm-12 col-xs-12">
-                    {!! Adsense::show('responsive') !!}
+            <div class="container">
+                <div class="row g-4">
+                    <div class="col-lg-12 col-sm-12 col-xs-12">
+                        {!! Adsense::show('responsive') !!}
+                    </div>
                 </div>
             </div>
         </section>
