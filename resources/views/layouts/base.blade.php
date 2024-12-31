ddc8414f665eb7ae0833cf2fa9250e50d0a302a9
MANOUDEV <manouadjanor@gmail.com>
2024-12-31 04:31:17 +0000

    [ADD] Affichage des pages de toutes les catégories, de recherche d'article, des articles par tags, par auteurs et par catégories.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -20,6 +20,7 @@
         <!-- Theme CSS -->
         <link id="style-switch" rel="stylesheet" type="text/css" href="/assets/css/style.css">
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5594963864359932" crossorigin="anonymous"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
 
