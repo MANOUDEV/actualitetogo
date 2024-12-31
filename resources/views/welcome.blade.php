79633e8fd86bdfae555fa603fdebd8989300234c
MANOUDEV <manouadjanor@gmail.com>
2024-12-31 18:34:07 +0000

    [ADD] Intégration google adsence

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,17 +25,7 @@
         </section>
         <div id="home_page"></div>
         <section class="position-relative container" style="margin-top: -95px; margin-bottom: -25px"> 
-        <!-- Annonce Google AdSense -->
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <ins class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-5594963864359932"
-            data-ad-slot="4473479121"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+         
         @include('sectionHomePage.toutelActualite.toutelActualite') 
             <!-- Annonce Google AdSense -->
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
